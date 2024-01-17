3da699cc33dbea222f9b3a5a1cabe74e5918feb4
Hari-Pant <120258224+Hari-Pant@users.noreply.github.com>
2024-01-17 15:01:06 +0545

    Update annually.php

--- annually.php
+++ annually.php
@@ -15,41 +15,6 @@
 
 <body>
 
-  <!-- <header class="text-gray-600 body-font bg-zinc-900">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
-          stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-          viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span class="ml-3 text-white text-xl">H-Fitness</span>
-      </a>
-      <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-        <a href="index.html" class="mr-5  text-orange-500">Home</a>
-        <a href="services.html" class="mr-5 text-white hover:text-orange-500">Services</a>
-        <a href="about us.html" class="mr-5 text-white hover:text-orange-500">About Us</a>
-        <a href="contact us.html" class="mr-5 text-white hover:text-orange-500">Contact</a>
-      </nav>
-      <button
-        class="inline-flex mr-3 items-center bg-gray-800 text-orange-500 border-0 py-1 px-3 focus:outline-none hover:text-white rounded text-base mt-4 md:mt-0"> <a href="login.html">Login</a> 
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-          class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-          <path d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg>
-      </button>
-
-      <button
-        class="inline-flex items-center bg-gray-800 text-orange-500 border-0 py-1 px-3 focus:outline-none hover:text-white rounded text-base mt-4 md:mt-0"> <a href="register.html">Register</a> 
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-          class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-          <path d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg>
-      </button>
-    </div>
-  </header> -->
-
 
   <nav class="bg-zinc-900 border-gray-200 px-2 md:px-4  py-2.5 dark:bg-gray-900">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
